5a6189138bad2e4b3cbc9f5e178875ea1c896eb4
Faristol <joseptortosa33@gmail.com>
2023-10-07 00:38:05 +0200

    poc a poc

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ import { Knight } from "./piecesobjects/knight.js";
 import { Pawn } from "./piecesobjects/pawn.js";
 import { Queen } from "./piecesobjects/queen.js";
 import { Rook } from "./piecesobjects/rook.js";
-import {PieceFather} from "./piecesobjects/piecefather.js";
+import { PieceFather } from "./piecesobjects/piecefather.js";
 
 let movementTarget = [];
 let gameState = {
-    piecesAlive : [],
-    start : false,
-    /* aci no estaria demés fer varies variables
+  piecesAlive: [],
+  start: false,
+  /* aci no estaria demés fer varies variables
     relatives a:
     ->El rey esta en jaque?
     ->AQuesta variable controlarà tots els altres moviments, abans de tot,
@@ -19,73 +19,94 @@ let gameState = {
     
     
     */
-    players: {
-        'white':'pepe',
-        'black':'paco',
-    },
-    piecesDead:[],
-    turn: 'white',
+  players: {
+    white: "pepe",
+    black: "paco",
+  },
+  piecesDead: [],
+  turn: "white",
 };
 
-document.addEventListener("DOMContentLoaded",start);
-function start(){
-    /*create table and put pieces*/
-    
-    createTablePieces();
-    /*game();*/
+document.addEventListener("DOMContentLoaded", start);
+function start() {
+  /*create table and put pieces*/
 
+  createTablePieces();
+  /*game();*/
 }
-function createTablePieces(){
-    let chessBoard = document.querySelector("#chessboard");
-    /*make a array of constructors*/
-    let orderPiecesConstructors = [Rook,Knight,Bishop,King,Queen,Bishop,Knight,Rook];
-    /*black top white down*/
-    let charCode = 'a'.charCodeAt(0);
-   for(let i=0;i<8;i++){
-    let row = 8-i;
+function createTablePieces() {
+  let chessBoard = document.querySelector("#chessboard");
+  /*make a array of constructors*/
+  let orderPiecesConstructors = [
+    Rook,
+    Knight,
+    Bishop,
+    King,
+    Queen,
+    Bishop,
+    Knight,
+    Rook,
+  ];
+  /*black top white down*/
+  let charCode = "a".charCodeAt(0);
+  for (let i = 0; i < 8; i++) {
+    let row = 8 - i;
     /*if i%2==0 true (starts with green) else false (starts with white)*/
-    let greenStart = i%2===0;
-    for (let j=0;j<8;j++){
-        /*if greenStart = true -> put green when j%2===0
+    let greenStart = i % 2 === 0;
+    for (let j = 0; j < 8; j++) {
+      /*if greenStart = true -> put green when j%2===0
         -> false: put white when j%2===0
         */
-        let color = greenStart?(j%2===0?"green":"white"):(j%2===0?"white":"green");
-        let column = String.fromCharCode((charCode+j));
-        let square = document.createElement("span");
-        square.classList.add(color);
-        square.classList.add("square");
-        square.addEventListener("click",captureAction);
-        
-        if(i===0){
-            /* rock,knight,bishop,king,queen,bishop,knight,rock: black*/
-            gameState.piecesAlive.push(new orderPiecesConstructors[j]('black',column+row));
-            square.classList
-        }else if(i===1){
-            /*pawn: black*/
-            gameState.piecesAlive.push(new Pawn('black',column+row));
-        }else if(i===6){
-            /*pawn: white*/
-            gameState.piecesAlive.push(new Pawn('white',column+row));
-        }else if(i===7){
-            /* rock,knight,bishop,king,queen,bishop,knight,rock: white*/
-            gameState.piecesAlive.push(new orderPiecesConstructors[j]('white',column+row));
-        }
-         if([0,1,6,7].includes(i)){
-            let index = gameState.piecesAlive.length-1;
-            
-            let unicodeValuePiece = gameState.piecesAlive[index].unicodePiece;
-            /*
+      let color = greenStart
+        ? j % 2 === 0
+          ? "green"
+          : "white"
+        : j % 2 === 0
+        ? "white"
+        : "green";
+      let column = String.fromCharCode(charCode + j);
+      let square = document.createElement("span");
+      square.classList.add(color);
+      square.classList.add("square");
+      square.addEventListener("click", captureAction);
+
+      if (i === 0) {
+        /* rock,knight,bishop,king,queen,bishop,knight,rock: black*/
+        gameState.piecesAlive.push(
+          new orderPiecesConstructors[j]("black", column + row)
+        );
+        square.classList;
+      } else if (i === 1) {
+        /*pawn: black*/
+        gameState.piecesAlive.push(new Pawn("black", column + row));
+      } else if (i === 6) {
+        /*pawn: white*/
+        gameState.piecesAlive.push(new Pawn("white", column + row));
+      } else if (i === 7) {
+        /* rock,knight,bishop,king,queen,bishop,knight,rock: white*/
+        gameState.piecesAlive.push(
+          new orderPiecesConstructors[j]("white", column + row)
+        );
+      }
+      if ([0, 1, 6, 7].includes(i)) {
+        let index = gameState.piecesAlive.length - 1;
+
+        let unicodeValuePiece = gameState.piecesAlive[index].unicodePiece;
+        /*
             let image = document.createElement("img");
             image.classList.add("piece");
             image.src = srcImage;
             image.id = column+row;*/
-            square.id=gameState.piecesAlive[index].type+gameState.piecesAlive[index].color+"_";
-            square.textContent=unicodeValuePiece;
-        }
-        square.id += column+row;
-        chessBoard.appendChild(square);
+        square.id =
+          gameState.piecesAlive[index].type +
+          gameState.piecesAlive[index].color +
+          "_";
+        square.textContent = unicodeValuePiece;
+      }
+      square.id += column + row;
+      chessBoard.appendChild(square);
     }
-   }
+  }
 }
 /*El joc començarà quan el jugador de les blanques faça un moviment valid, açò comporta
 1) clickar sobre la peça
@@ -96,27 +117,28 @@ Aquesta peça pot fer aquest moviment?
 
 Sistema de moviment-> click-click s'haura de tindre en compte que el destí 
 no siga la mateixa posicio a l'actual
-*/ 
+*/
 /*de moment sols posarem dos condicions-> destí no siga el mateix a la posició actual
 i que no hi haja cap peça seua en la casella destí*/
-function captureAction(e){
-    e.stopPropagation();
-    let element = e.target;
-    /*si l'array capturador d'events esta buit i a més el contingut de la casella es diferent a 0, bingo, a ha marcat una peça, 
+function captureAction(e) {
+  e.stopPropagation();
+  let element = e.target;
+  /*si l'array capturador d'events esta buit i a més el contingut de la casella es diferent a 0, bingo, a ha marcat una peça, 
     serà correcta? */
-    if(element.textContent.length!==0&&movementTarget.length===0){
-        if(element.id.includes(gameState.turn)){
-            /*ha elegit una peça que correspon en color al torn*/
-            /*guardem el e.target al movementTarget*/
-            movementTarget.push(element);
-        }
-        /*  quines condicions elementals s'han de complir per a que el següent moviment siga 
+  if (element.textContent.length !== 0 && movementTarget.length === 0) {
+    if (element.id.includes(gameState.turn)) {
+      /*ha elegit una peça que correspon en color al torn*/
+      /*guardem el e.target al movementTarget*/
+      movementTarget.push(element);
+    }
+    /*  quines condicions elementals s'han de complir per a que el següent moviment siga 
         candidat a validar?*/
-        /*A)El escac marcat esta buit
+    /*A)El escac marcat esta buit
         B) L'escac marcat esta ocupat per una peca de color opost
         */
-    }else if(element.textContent.length===0&&movementTarget.length===1){
-        /*Condicio A
+  } else {
+    if (element.textContent.length === 0 && movementTarget.length === 1) {
+      /*Condicio A
         si es esta buida, passarem a validar el moviment tinguent en compte
         posicio inicial, posicio final, regles propies de la peça
         de moment la mourem i iau
@@ -126,12 +148,17 @@ function captureAction(e){
         //TO DO
         fer una funcio isMovementValid() per a cada peça
         */
-       movementTarget.push(element);
-       movePiece(movementTarget);
-       movementTarget = [];
-       changeTurn();
-    }else if(element.textContent.length!==0&&movementTarget.length===1){
-        /*aci aprofitarem i controlarem dos casos
+      movementTarget.push(element);
+      movePiece(movementTarget);
+      movementTarget = [];
+      changeTurn();
+    } else if (
+      element.textContent.length !== 0 &&
+      movementTarget.length === 1
+    ) {
+      console.log("hola");
+
+      /*aci aprofitarem i controlarem dos casos
 
         1)Si el segon click el fa sobre una peça del seu color-> no passa absolutament res
         movementTarget = [];
@@ -142,86 +169,80 @@ function captureAction(e){
         2)Si el segon click el fa sobre una peça de color opost(, de moment no controlarem si es valid el moviment
         , si fos invalid, passaria com el cas (1),) la mata, s'actualitza la funció pieces alive i s'afegix a les peces mortes
         */
-       if(movementTarget[0].id.includes(gameState.turn)){
+      if (movementTarget[0].id.includes(gameState.turn)) {
+        console.log("hola");
         /*clavar condicio per a veure si es un enroc*/
 
-
-        /**/ 
-        movementTarget=[];
-       }else if(!movementTarget[0].id.includes(gameState.turn)){
+        /**/
+        movementTarget = [];
+      } else {
         /*peça de color opost*/
-        
+        console.log("hola");
+
         movementTarget.push(element);
         killPiece(movementTarget);
         movementTarget = [];
-       changeTurn();
-
-       }
-
+        changeTurn();
+      }
     }
-    
-
+  }
 }
-function game(){
-    /*enableDisableMovementPlayerColor((gameState.turn==='white'?'white':'black'),(gameState.turn==='white'?'black':'white'));*/
-
+function game() {
+  /*enableDisableMovementPlayerColor((gameState.turn==='white'?'white':'black'),(gameState.turn==='white'?'black':'white'));*/
 }
-function enableDisableMovementPlayerColor(colorEnableMove,colorDisableMove){
-    /*fer que soles es poden moure les blanques o negres, segons el torn*/
-    /*al primer torn, per cocos, haura de fer click en una peca del seu color, el segon click no, pot ser avançe a un escac buit o ple*/
-    /*aço mes avant quan introduïm el tema d'usuaris i tal*/
-    /*si es poguera diferenciar el clik del usuari1 i el click del u2, estaria collonut*/
-    
-
-
+function enableDisableMovementPlayerColor(colorEnableMove, colorDisableMove) {
+  /*fer que soles es poden moure les blanques o negres, segons el torn*/
+  /*al primer torn, per cocos, haura de fer click en una peca del seu color, el segon click no, pot ser avançe a un escac buit o ple*/
+  /*aço mes avant quan introduïm el tema d'usuaris i tal*/
+  /*si es poguera diferenciar el clik del usuari1 i el click del u2, estaria collonut*/
 }
-function changeTurn(){
-    gameState.turn=(gameState.turn==='white'?'black':'white');
+function changeTurn() {
+  gameState.turn = gameState.turn === "white" ? "black" : "white";
 }
-function movePiece(movementTarget){
-    /*primer element posicio inicial*/
-    let pieceToMove = movementTarget[0];
-    let destination = movementTarget[1];
-    /*atributs i estructura de dades a tindre en compte*/
-    /*id i textContent*/
-    let idPieceToMove = pieceToMove.id;
-    let idPieceToMoveWhitoutPiece = idPieceToMove.split("_")[1];
-    let idPieceName = idPieceToMove.split("_")[0];
-    let unicodePieceToMove = pieceToMove.textContent;
-    /*canviem el nom de la peça a l'escac i li llevem el unicode*/
-    pieceToMove.id = idPieceToMoveWhitoutPiece;
-    pieceToMove.textContent = "";
-    /*li posem l'unicode i el nou id a destination*/
-    destination.id = idPieceName+"_"+destination.id;
-    destination.textContent=unicodePieceToMove;
-    refreshPositionPiecesAlive(idPieceToMoveWhitoutPiece,destination.id.split("_")[1]);
-    
-
-
+function movePiece(movementTarget) {
+  /*primer element posicio inicial*/
+  let pieceToMove = movementTarget[0];
+  let destination = movementTarget[1];
+  /*atributs i estructura de dades a tindre en compte*/
+  /*id i textContent*/
+  let idPieceToMove = pieceToMove.id;
+  let idPieceToMoveWhitoutPiece = idPieceToMove.split("_")[1];
+  let idPieceName = idPieceToMove.split("_")[0];
+  let unicodePieceToMove = pieceToMove.textContent;
+  /*canviem el nom de la peça a l'escac i li llevem el unicode*/
+  pieceToMove.id = idPieceToMoveWhitoutPiece;
+  pieceToMove.textContent = "";
+  /*li posem l'unicode i el nou id a destination*/
+  destination.id = idPieceName + "_" + destination.id;
+  destination.textContent = unicodePieceToMove;
+  refreshPositionPiecesAlive(
+    idPieceToMoveWhitoutPiece,
+    destination.id.split("_")[1]
+  );
 }
-function killPiece(movementTarget){
-    let pieceKiller = movementTarget[0];
-    let pieceToKill = movementTarget[1];
+function killPiece(movementTarget) {
+  let pieceKiller = movementTarget[0];
+  let pieceToKill = movementTarget[1];
 
-    let idPieceKiller = pieceKiller.id;
-    let unicodePieceKiller = pieceKiller.textContent;
-    
+  let idPieceKiller = pieceKiller.id;
+  let unicodePieceKiller = pieceKiller.textContent;
 
-    let idPieceToKill = pieceToKill.id;
+  let idPieceToKill = pieceToKill.id;
 
-    /*canviem el id de la pieceKiller pel seu sense el nom*/
-    pieceKiller.id = idPieceKiller.substring(idPieceKiller.length-2);
-    pieceKiller.textContent = "";
+  /*canviem el id de la pieceKiller pel seu sense el nom*/
+  pieceKiller.id = idPieceKiller.substring(idPieceKiller.length - 2);
+  pieceKiller.textContent = "";
 
-    pieceToKill.id= idPieceKiller.split("_")[0]+"_"+idPieceToKill.substring(idPieceToKill.length-2);
-    pieveToKill.textContent=unicodePieceKiller;
-    
-
-    
-
+  pieceToKill.id =
+    idPieceKiller.split("_")[0] +
+    "_" +
+    idPieceToKill.substring(idPieceToKill.length - 2);
+  pieceToKill.textContent = unicodePieceKiller;
 }
-function refreshPositionPiecesAlive(idDestination0,idDestinationF){
-    /*actualitzem l'element mogut a la nova coordenada*/
-    let index = gameState.piecesAlive.findIndex((piece) => piece.coordinates == idDestination0);
-    if(index!==-1) gameState.piecesAlive[index].coordinates = idDestinationF;
+function refreshPositionPiecesAlive(idDestination0, idDestinationF) {
+  /*actualitzem l'element mogut a la nova coordenada*/
+  let index = gameState.piecesAlive.findIndex(
+    (piece) => piece.coordinates == idDestination0
+  );
+  if (index !== -1) gameState.piecesAlive[index].coordinates = idDestinationF;
 }
