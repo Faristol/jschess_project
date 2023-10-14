6d7171148eb6901e14f7a776ae9abfb48bdb0764
Faristol <joseptortosa33@gmail.com>
2023-10-14 22:08:29 +0200

    implementacio check casi acabada

--- checkdetection.js
+++ checkdetection.js
@@ -65,6 +65,7 @@ function isKingCheck(turn) {
   let isChecked = false;
   isChecked = piecesMovementHandler(piecesOpponent, king, isChecked);
 }
+
 function piecesMovementHandler(piecesOpponent, king, isChecked) {
   piecesOpponent.forEach((piece) => {
     switch (piece.type) {
@@ -78,26 +79,244 @@ function piecesMovementHandler(piecesOpponent, king, isChecked) {
             break;
           }
         } else {
-            if (
-                traceDiagonalDescendentLeftToRight(piece.coordinates, 1) ||
-                traceDiagonalDescendentRightToLeft(piece.coordinates, 1)
-              ) {
-                isChecked = true;
-                break;
-              }
+          if (
+            traceDiagonalDescendentLeftToRight(piece.coordinates, 1) ||
+            traceDiagonalDescendentRightToLeft(piece.coordinates, 1)
+          ) {
+            isChecked = true;
+            break;
+          }
         }
 
         break;
       case "knight":
-        if()
+        if (tracePositionsKnight(piece.coordinates)) {
+          isChecked = true;
+          break;
+        }
         break;
       case "bishop":
+        if (
+          traceDiagonalAscendentLeftToRight(piece.coordinates) ||
+          traceDiagonalAscendentRightToLeft(piece.coordinates) ||
+          traceDiagonalDescendentLeftToRight(piece.coordinates) ||
+          traceDiagonalDescendentRightToLeft(piece.coordinates)
+        ) {
+          isChecked = true;
+          break;
+        }
         break;
       case "queen":
+        if (
+          traceDiagonalAscendentLeftToRight(piece.coordinates) ||
+          traceDiagonalAscendentRightToLeft(piece.coordinates) ||
+          traceDiagonalDescendentLeftToRight(piece.coordinates) ||
+          traceDiagonalDescendentRightToLeft(piece.coordinates) ||
+          traceVerticalAscendent(piece.coordinates) ||
+          traceVerticalDescendent(piece.coordinates) ||
+          traceHorizontalLeftToRight(piece.coordinates) ||
+          traceHorizontalRightToLeft(piece.coordinates)
+        ) {
+          isChecked = true;
+          break;
+        }
+
         break;
       case "rook":
+        if (
+          traceHorizontalLeftToRight(piece.coordinates) ||
+          traceHorizontalRightToLeft(piece.coordinates) ||
+          traceVerticalAscendent(piece.coordinates) ||
+          traceVerticalDescendent(piece.coordinates)
+        ) {
+          isChecked = true;
+          break;
+        }
         break;
     }
   });
   return isChecked;
+}
+//idea: en funcio de la funcio, valga la redundància, obtindre un array de posicions des de l'actual, fins al límit
+//filtrar els elements per la posicio
+//ordenarlos en funcio de l'ordre del rang
+//agafar el primer objecte-> si es rey -> check
+function traceVerticalAscendent(coordinates) {
+  /*lletra constant numero augmenta*/
+  let position = coordinates;
+  const letter = position.split("")[0];
+  let num = parseInt(position.split("")[1]);
+  let verticalAscendentRange = [];
+  while (!verticalAscendentRange.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    ++num;
+    verticalAscendentRange.push(letter + num);
+  }
+  /*llevem l'ultim element de l'array*/
+  if (verticalAscendentRange.length > 0) {
+    verticalAscendentRange.pop();
+  }
+
+  return filterOrderAndGetFirstElement(verticalAscendentRange);
+}
+function traceVerticalDescendent(coordinates) {
+  //letra constant numero disminueix
+  let position = coordinates;
+  const letter = position.split("")[0];
+  let num = parseInt(position.split("")[1]);
+  let verticalDescendentRange = [];
+  while (!verticalDescendentRange.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    --num;
+    verticalDescendentRange.push(letter + num);
+  }
+  /*llevem l'ultim element de l'array*/
+  //si sols te un element serà un limit
+  if (verticalDescendentRange.length > 0) {
+    verticalDescendentRange.pop();
+  }
+  return filterOrderAndGetFirstElement(verticalDescendentRange);
+}
+function traceHorizontalLeftToRight(coordinates) {
+  //num constant lletra augmenta
+  let position = coordinates;
+  let letter = position.split("")[0].charCodeAt(0);
+  const num = position.split("")[1];
+
+  let horizontalLeftToRightRange = [];
+
+  while (!horizontalLeftToRightRange.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    ++letter;
+    horizontalLeftToRightRange.push(String.fromCharCode(letter) + num);
+  }
+  if (horizontalLeftToRightRange.length > 0) {
+    horizontalLeftToRightRange.pop();
+  }
+  return filterOrderAndGetFirstElement(horizontalLeftToRightRange);
+}
+function traceHorizontalRightToLeft(coordinates) {
+  //num constant lletra disminueix
+  let position = coordinates;
+  let letter = position.split("")[0].charCodeAt(0);
+  const num = position.split("")[1];
+
+  let horizontalRightToLeftRange = [];
+
+  while (!horizontalRightToLeftRange.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    --letter;
+    horizontalRightToLeftRange.push(String.fromCharCode(letter) + num);
+  }
+  if (horizontalRightToLeftRange.length > 0) {
+    horizontalRightToLeftRange.pop();
+  }
+  return filterOrderAndGetFirstElement(horizontalRightToLeftRange);
+}
+function traceDiagonalAscendentLeftToRight(coordinates) {
+  //el tema del control del jaque amb peons el controlarem a ma i iau
+  //lletra ++ num ++
+  let position = coordinates;
+  let letter = position.split("")[0].charCodeAt(0);
+  let num = parseInt(position.split("")[1]);
+
+  let diagonalAscendentLeftToRight = [];
+
+  while (!diagonalAscendentLeftToRight.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    ++letter;
+    ++num;
+    diagonalAscendentLeftToRight.push(String.fromCharCode(letter) + num);
+  }
+
+  if (diagonalAscendentLeftToRight.length > 0) {
+    diagonalAscendentLeftToRight.pop();
+  }
+  return filterOrderAndGetFirstElement(diagonalAscendentLeftToRight);
+}
+function traceDiagonalAscendentRightToLeft(coordinates) {
+  //lletra -- num ++
+  let position = coordinates;
+  let letter = position.split("")[0].charCodeAt(0);
+  let num = parseInt(position.split("")[1]);
+
+  let diagonalAscendentRightToLeft = [];
+
+  while (!diagonalAscendentRightToLeft.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    --letter;
+    ++num;
+    diagonalAscendentRightToLeft.push(String.fromCharCode(letter) + num);
+  }
+
+  if (diagonalAscendentRightToLeft.length > 0) {
+    diagonalAscendentRightToLeft.pop();
+  }
+  return filterOrderAndGetFirstElement(diagonalAscendentRightToLeft);
+}
+function traceDiagonalDescendentLeftToRight(coordinates) {
+  //lletra++ num --
+
+  let position = coordinates;
+  let letter = position.split("")[0].charCodeAt(0);
+  let num = parseInt(position.split("")[1]);
+
+  let diagonalDescendentLeftToRight = [];
+
+  while (!diagonalDescendentLeftToRight.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    ++letter;
+    --num;
+    diagonalDescendentLeftToRight.push(String.fromCharCode(letter) + num);
+  }
+
+  if (diagonalDescendentLeftToRight.length > 0) {
+    diagonalDescendentLeftToRight.pop();
+  }
+  return filterOrderAndGetFirstElement(diagonalDescendentLeftToRight);
+}
+function traceDiagonalDescendentRightToLeft(coordinates) {
+  //lletra -- num --
+
+  let position = coordinates;
+  let letter = position.split("")[0].charCodeAt(0);
+  let num = parseInt(position.split("")[1]);
+
+  let diagonalDescendentRightToLeft = [];
+
+  while (!diagonalDescendentRightToLeft.some((xy) => limits.includes(xy))) {
+    //en el moment que hi haja un element de limits inclos en verticalAscendentRange pararà, i l'ultim element serà el limit. deurem llevarlo
+    --letter;
+    --num;
+    diagonalDescendentRightToLeft.push(String.fromCharCode(letter) + num);
+  }
+
+  if (diagonalDescendentRightToLeft.length > 0) {
+    diagonalDescendentRightToLeft.pop();
+  }
+  return filterOrderAndGetFirstElement(diagonalDescendentRightToLeft);
+}
+function tracePositionsKnight(coordinates) {}
+function filterOrderAndGetFirstElement(range) {
+  if (range > 0) {
+    let piecesInRange = gameState.piecesAlive.filter((piece) =>
+      range.includes(piece.coordinates)
+    );
+    //ara les ordenem
+    let piecesInRangeSorted = [];
+    for (const xy of range) {
+      for (const piece of piecesInRange) {
+        if (xy === piece.coordinates) {
+          piecesInRangeSorted.push(piece);
+          break;
+        }
+      }
+    }
+    //ara agafem la primera posicio de l'array de peces ordenat
+    return (
+      piecesInRangeSorted[0].type === "king" &&
+      piecesInRangeSorted[0].color === gameState.turn
+    );
+  }
+  return false;
 }
